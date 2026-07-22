b8eb6e04debfbdd0dc5b458053c1729f44ab39e5
PickAndStats <pickandstats@gmail.com>
2026-07-22 23:06:12 +0200

    Fuentes de la app servidas en local con Fontsource y favicon unificado en todo el dominio

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,3 +1,9 @@
+import '@fontsource/barlow/400.css';
+import '@fontsource/barlow/600.css';
+import '@fontsource/barlow/700.css';
+import '@fontsource/barlow-condensed/500.css';
+import '@fontsource/barlow-condensed/600.css';
+import '@fontsource/barlow-condensed/700.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
